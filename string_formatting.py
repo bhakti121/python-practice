76df45237906739c275126ba390181094c499aa1
unknown <bhaktichandilkar@gmail.com>
2025-10-04 18:19:53 +0530

    learn string formatting

---
+++ string_formatting.py
@@ -0,0 +1,55 @@
+name = "Bob"
+greeting = "Hello, Bob"
+
+print(greeting)
+
+name = "Rolf"
+
+print(greeting)
+
+greeting = f"Hello, {name}"
+print(greeting)
+
+# --
+
+name = "Anne"
+print(
+    greeting
+)  # This still prints "Hello, Rolf" because `greeting` was calculated earlier.
+print(
+    f"Hello, {name}"
+)  # This is correct, since it uses `name` at the current point in time.
+
+# -- Using .format() --
+
+# We can define template strings and then replace parts of it with another value, instead of doing it directly in the string.
+
+greeting = "Hello, {}"
+with_name = greeting.format("Rolf")
+print(with_name)
+
+longer_phrase = "Hello, {}. Today is {}."
+formatted = longer_phrase.format("Rolf", "Monday")
+
+print(formatted)
+
+
+
+#practice
+
+name="Namdev"
+greeting="Hello, {}"
+print(greeting.format(name))
+
+name="Alice"
+greeting=f"Hello, {name}"
+print(greeting)
+
+
+age=24
+greeting=f"hello,{name},you are {age} years old"
+print(greeting)
+
+age=30
+name="namdev"
+print("hello,{},you are {} years old".format(name,age))
